3b6dabab0e01a36e7455c8a729a5cd98ef94c344
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-10 14:34:42 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Этот файл является частью виджета информационной панели (Dashboard).
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Components',
+    '{description}' => 'Components available to the user',
+
+    // Options: поля
+    'List items' => 'List items',
+    'Modules' => 'Modules',
+    'Extensions' => 'Extensions',
+    'Show title' => 'Show title',
+    'Show description' => 'Show description',
+    'Compact list view' => 'Compact list view',
+    'Modules displayed in the list' => 'Modules displayed in the list',
+    'Extensions displayed in the list' => 'Extensions displayed in the list'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Этот файл является частью виджета информационной панели (Dashboard).
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Компоненты',
+    '{description}' => 'Компоненты доступные пользователю',
+
+    // Options: поля
+    'List items' => 'Элементы списка',
+    'Modules' => 'Модули',
+    'Extensions' => 'Расширения',
+    'Show title' => 'Показывать заголовок',
+    'Show description' => 'Показывать описание',
+    'Compact list view' => 'Компактный вид списка',
+    'Modules displayed in the list' => 'Модули отображаемые в списке',
+    'Extensions displayed in the list' => 'Расширения отображаемые в списке'
+];
